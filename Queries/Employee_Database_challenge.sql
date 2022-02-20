7b9e2de6ef540c9876ae8fd1fae393e8d7f9c021
ZForadi <96031882+ZForadi@users.noreply.github.com>
2022-02-19 19:22:56 -0500

    Adding Additional Queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -47,3 +47,49 @@ ON (e.emp_no = t.emp_no)
 WHERE (de.to_date = '9999-01-01')
      AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY emp_no;
+
+-- Department Retirements
+SELECT di.dept_name, COUNT(ut.emp_no)
+INTO dept_retiring
+FROM unique_titles as ut
+LEFT JOIN dept_info as di
+ON ut.emp_no = di.emp_no
+GROUP BY di.dept_name
+ORDER BY di.dept_name;
+
+-- By Department - unique Titles Counts 
+SELECT di.dept_name, 
+ut.title,
+COUNT(ut.emp_no)
+INTO dept_retiring_titles
+FROM unique_titles as ut
+INNER JOIN dept_info as di
+ON (ut.emp_no = di.emp_no)
+GROUP BY di.dept_name, ut.title
+ORDER BY di.dept_name DESC;
+
+-- By Department Mentors 
+SELECT d.dept_name, de.dept_no,COUNT(me.emp_no)
+INTO dept_mentors
+FROM dept_employees as de
+RIGHT JOIN mentorship_eligibility as me
+ON me.emp_no = de.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
+GROUP BY d.dept_name, de.dept_no
+ORDER BY d.dept_name;
+
+
+-- By Department Mentors Titles 
+SELECT d.dept_name, de.dept_no,
+me.title,
+COUNT(me.emp_no)
+INTO dept_mentors_titles
+FROM dept_employees as de
+RIGHT JOIN mentorship_eligibility as me
+ON me.emp_no = de.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
+GROUP BY d.dept_name, de.dept_no, me.title
+ORDER BY d.dept_name DESC;
+SELECT * FROM dept_mentors_titles;
